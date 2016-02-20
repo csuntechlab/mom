01a2b4465a8d4a08951657073c67aaba3fd02ed0
LethalLima <jeffrey.i.lima@gmail.com>
2016-02-19 23:35:37 -0800

    Created NemoEntity model to store META+Lab projects.

---
+++ app/Models/NemoEntity.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mom\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NemoEntity extends Model
+{
+   protected $table = 'nemo.entities';
+   protected $primaryKey = 'entities_id';
+   public $incrementing = false;
+   protected $fillable = [
+      'entities_id', 
+      'parent_entities_id', 
+      'display_name', 
+      'description',
+   ];
+}
