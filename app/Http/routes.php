09683b417f0a9143f7393f668a310e004c6da7ad
Georgiy Danielyan <georgiyd91@gmail.com>
2016-02-22 15:20:15 -0800

    removed dummy routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,27 +15,7 @@ Route::get('/', function () {
 });
 
 Route::get('profile', function() {
-   // returns profile for that user -> This WORKS!
-   // $user = Mom\Models\User::find('members:104733445');
-   // $profile = $user->profile()->get();
-   // return $profile;
-
-   // returns links for that profile -> This WORKS!
-   // $links = Mom\Models\Profile::where('individuals_id', 'members:104733445')->with('links')->get();
-   // return $links;
-      // OR
-   // $user = Mom\Models\Profile::find('members:104733445');
-   // $links = $user->links()->get();
-   // return $links;
-
-   // returns the full name of the user.
-   // $user = Mom\Models\Profile::find('members:104733445');
-   // $name = $user->fullName();
-   // return $name;
-
-   // $user = Mom\Models\User::all();
-   // $user->load('profile.links', 'profile.skills');
-   // return $user;
+   return Mom\Models\Profile::all();
 });
 
 // test route for Project Model
@@ -43,11 +23,6 @@ Route::get('projects', function(){
    return Mom\Models\Project::all();
 });
 
-// test route for NemoEntity Model to return all projects of META+Lab
-// NemoEntity Model will be used to create the projects and the Project model to display and edit them.
-Route::get('entities', function(){
-   return Mom\Models\NemoEntity::where('parent_entities_id', 'departments:10390')->where('entity_type', 'Project')->get();
-});
 /*
 |--------------------------------------------------------------------------
 | Application Routes
