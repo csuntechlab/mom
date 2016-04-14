9073e7b2c55a568123675f1f9c37ba6b35f8388d
Arvin <arvin.flores@metalab.csun.edu>
2016-04-14 12:46:16 -0700

    fixed link display issues on edit-student for other user profiles

--- resources/views/pages/profiles/edit-student.blade.php
+++ resources/views/pages/profiles/edit-student.blade.php
@@ -179,19 +179,7 @@
 			    <div>
 			      <div class="modal-body student lines-back">
 			        <div class="row">
-			        	<div class="col-xs-1"></div>
-			        	<div class="col-xs-2 text-right">
-			        		<div class="social-media-btn">
-			        		@if(isset($portfolium_url))
-			        			<a href="{{ ($portfolium_url) }}">
-			        				<img class="social-media-btn-img" src="{{ asset('imgs/Hover.svg' ) }}" alt="portfolium" />
-			        				<p>Portfolium</p>
-			    				</a>
-			    			@endif
-							</div>
-						</div>
-						
-			        	<div class="col-xs-6 text-center">
+			        	<div class="text-center">
 			        	@if(isset($profile->image->src))
 						<div id="profile_image_preview" style="border-radius: 50%; width: 200px; height: 200px; background: url('{{ asset('user-profile/image/' . $profile->image->src) }}') no-repeat center center; background-size: cover; position: relative;">
 				    	</div>
@@ -200,19 +188,29 @@
 				    	</div>
 				    	@endif
 			        	</div>
-			        	<div class="col-xs-2 text-left">
-			        		<div class="social-media-btn">
-			        		@if(isset($linkedin_url))
-			        			<a href="{{ $linkedin_url }}">
-			        				<img class="social-media-btn-img" src="{{ asset('imgs/linkden.svg' ) }}" alt="linkedin" />
-			        				<p>LinkedIn</p>
-			    				</a>
-			    			@endif
-							</div>
-						</div>
-			        	<div class="col-md-1"></div>
+			        	@if(isset($linkedin_url) || isset($portfolium_url))
+			        	<div class="row" style="text-align: center; margin-top: 25px;">
+			        		<ul class="list-inline list-unstyled">
+			        			@if(isset($linkedin_url))
+			        			<li>
+				        			<a href="{{ $linkedin_url }}">
+				        				<img class="social-media-btn-img" src="{{ asset('imgs/linkden.svg' ) }}" alt="linkedin" />
+				        				<p>LinkedIn</p>
+				    				</a>
+			    				</li>
+			        			@endif
+			        			@if(isset($portfolium_url))
+			        			<li>
+			        				<a href="{{ ($portfolium_url) }}">
+			        				<img class="social-media-btn-img" src="{{ asset('imgs/Hover.svg' ) }}" alt="portfolium" />
+			        				<p>Portfolium</p>
+			    					</a>
+			        			</li>
+			        			@endif
+			        		</ul>
+			        	</div>
+			        	@endif
 			        </div>
-			        
 			        <div class="row">
 			        	<div class="col-sm-12 text-center">
 			        		<p class="student-name">{{ $profile->fullName() }}</p>
