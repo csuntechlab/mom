637b9e5e0f0a582d2c49e82344c2748b2945e5a3
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-04-25 12:18:37 -0700

    fix the seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,5 @@ class DatabaseSeeder extends Seeder
    {
       $this->call(LinkTableSeeder::class);
       $this->call(ProfileTableSeeder::class);
-      $this->call(LinkProfileSeeder::class);
    }
 }
