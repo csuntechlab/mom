51c392bf7558174240754ebf21ac5d6b1336e3ae
Margrethe Moxnes <margrethe@moxnesdesign.no>
2016-05-11 10:34:30 -0700

    Added alt text to images

--- resources/views/pages/projects/work.blade.php
+++ resources/views/pages/projects/work.blade.php
@@ -30,9 +30,9 @@ Our Work
                                 <h2>{{ $project->meta->title }}</h2>
                                 <hr class="line-inline">
                                 @if(!empty($project->image))
-                                    <img src="{{ asset('imgs/projects/'.$project->image->src)}}" alt="Product image" class="img-responsive">
+                                    <img src="{{ asset('imgs/projects/'.$project->image->src)}}" alt="{{ $project->meta->title }}" class="img-responsive">
                                 @else
-                                    <img src="{{ asset('imgs/macbook-pro-placeholder.png')}}" alt="" class="img-responsive">
+                                    <img src="{{ asset('imgs/macbook-pro-placeholder.png')}}" alt="{{ $project->meta->title }}" class="img-responsive">
                                 @endif
 <!-- MOBILE DROP DOWN FUNCTIONS done -->
                                 <div class="row hidden-md hidden-lg hidden-xl">
@@ -70,16 +70,16 @@ Our Work
                                                     $productOwnerID = $project->productOwner->user_id; ?>  
                                                                                                   
                                                     @if(!empty($project->productOwner->profile) && !empty($project->productOwner->profile->image))
-                                                        <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->productOwner->profile->image->src)}}" alt="">
+                                                        <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->productOwner->profile->image->src)}}" alt="{{$project->productOwner->display_name}}">
                                                     @else
-                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="">
+                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->productOwner->display_name}}">
                                                     @endif                                                    
                                                 </div> 
                                                 <div class="col-xs-6 col-sm-6">                    
                                                     @if(!empty($project->scrumMaster->profile) && !empty($project->scrumMaster->profile->image))
                                                         <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->scrumMaster->profile->image->src)}}" alt="">
                                                     @else
-                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="">
+                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->scrumMaster->display_name}}">
                                                     @endif                                                    
                                                 </div>
                                                 <div class="col-xs-6 col-sm-6">
@@ -142,14 +142,14 @@ Our Work
                                     @if(!empty($project->productOwner->profile) && !empty($project->productOwner->profile->image))
                                         <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->productOwner->profile->image->src)}}" alt="">
                                     @else
-                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$member->display_name}}">
+                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->productOwner->display_name}}">
                                     @endif
                                     </div>  
                                     <div class="col-md-6">
                                     @if(!empty($project->scrumMaster->profile) && !empty($project->scrumMaster->profile->image))
                                         <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->scrumMaster->profile->image->src)}}" alt="">
                                     @else
-                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$member->display_name}}">
+                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->scrumMaster->display_name}}">
                                     @endif
                                     </div>
                                     <div class="col-md-6">
