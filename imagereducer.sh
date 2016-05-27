0eb3e36e897016e39cc5519b764fb24afeb3c036
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-05-27 16:50:55 -0700

    Fix the script a little need to fix it for project images.

--- imagereducer.sh
+++ imagereducer.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-imagereduce()
+profilereduce()
 {
 	for file in *;
 	do
-		if [[ ( $file == *.jpg ) || ( $file == *.png ) || ( $ifle == *.JPG ) || ( $file == *.PNG ) && ( $file != sm_* ) && ( $file != lg_* ) ]]
+		if [[ ( $file != sm_* ) && ( $file != lg_*) ]]
 			then
-				echo "$file"
+			if [[ ( $file == *.jpg ) || ( $file == *.png ) || ( $ifle == *.JPG ) ]]
+				then
 				convert "$file" -resize "$LARGE" "lg_$file" && convert "$file" -resize "$SMALL" "sm_$file" && rm "$file"
+			fi
 		fi
 	done
 }
 
 PROFILE_DIRECTORY="public/user-profile/image/"
+PROJECT_DIRECTORY="public/imgs/projects/"
 LARGE="200x175"
 SMALL="150x150"
 cd $PROFILE_DIRECTORY
+profilereduce
+cd ../
+cd $PROJECT_DIRECTORY
 imagereduce
 
