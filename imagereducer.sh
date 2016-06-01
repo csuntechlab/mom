fd4ceb66a32d6601b97de0d8da1e83ec84f1ce07
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-06-01 12:02:07 -0700

    The all important commit, add comments to code

--- imagereducer.sh
+++ imagereducer.sh
@@ -4,16 +4,20 @@ imagereduce()
 {
 	for file in *;
 	do
+		# lets ignore any placeholder images and images that have already been re-sized
 		if [[ ( $file != sm_* ) && ( $file != lg_*) && ( $file != *placeholder* ) ]]
 			then
+			# let's only traverse through image files
 			if [[ ( $file == *.jpeg ) || ( $ifle == *.JPEG ) || ( $file == *.jpg ) || ( $ifle == *.JPG ) || ( $file == *.png ) || ( $file == '*.PNG' ) ]]
 				then
 				# we are in the profile folder
 				if($flag == "true")
 					then
+					# make re-size twice and then we remove the original file
 					convert "$file" -resize "$LARGE" "lg_$file" && convert "$file" -resize "$SMALL" "sm_$file" && rm "$file"
 				# we are in the project folder
 				else
+					# re-size once and then we remove the original file
 					convert "$file" -resize "$LARGE" "lg_$file" && rm "$file"
 				fi
 			fi
