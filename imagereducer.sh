a74481e5860f90af2656583f44ea4d4565433eb7
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-06-03 12:30:39 -0700

    Fix the image reducer script for real this time.
    Remove profile creation from AuthController and only have it inside ProfileController

--- imagereducer.sh
+++ imagereducer.sh
@@ -10,16 +10,8 @@ imagereduce()
 			# let's only traverse through image files
 			if [[ ( $file == *.jpeg ) || ( $file == *.JPEG ) || ( $file == *.jpg ) || ( $file == *.JPG ) || ( $file == *.png ) || ( $file == '*.PNG' ) ]]
 				then
-				# we are in the profile folder
-				if($flag == "true")
-					then
-					# make re-size twice and then we remove the original file
-					convert "$file" -resize "$LARGE" "lg_$file" && convert "$file" -resize "$SMALL" "sm_$file" && rm "$file"
-				# we are in the project folder
-				else
-					# re-size once and then we remove the original file
-					convert "$file" -resize "$LARGE" "lg_$file" && rm "$file"
-				fi
+				# make re-size twice and then we remove the original file
+				convert "$file" -resize "$LARGE" "lg_$file" && convert "$file" -resize "$SMALL" "sm_$file" && rm "$file"
 			fi
 		fi
 	done
@@ -27,19 +19,19 @@ imagereduce()
 
 PROFILE_DIRECTORY="public/user-profile/image/"
 PROJECT_DIRECTORY="public/imgs/projects/"
-LARGE="200x175"
-SMALL="150x150"
-flag="true"
+LARGE="200x200"
+SMALL="50x50"
 echo "cd into $PROFILE_DIRECTORY"
 cd $PROFILE_DIRECTORY
 echo "inside $PROFILE_DIRECTORY"
-imagereduce $flag
+imagereduce
 echo "finished"
-flag="false"
 cd ../../../
+LARGE="200x175"
+SMALL="150x150"
 echo "cd into $PROJECT_DIRECTORY"
 cd $PROJECT_DIRECTORY
 echo "inside $PROJECT_DIRECTORY"
-imagereduce $flag
+imagereduce
 echo "finished"
 
