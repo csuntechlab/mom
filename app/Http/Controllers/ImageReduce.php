59ab4d9c3cf5dac99caeefbe4acb006aff67157f
Luis Guzman <luis.guzman@metalab.csun.edu>
2016-06-08 16:28:41 -0700

    Add comments to the code.
    Fixed an issue where files were being duplicated

--- app/Http/Controllers/ImageReduce.php
+++ app/Http/Controllers/ImageReduce.php
@@ -14,35 +14,65 @@ class ImageReduce extends Controller
 {
     use ImageHandler;
 
-    private $path;
-
-    private function resize($image, $profile = false, $fileName)
+    /**
+     * Here is where the resizing of the images takes place
+     * @param  File $image the full path of where the image is
+     * @param  boolean $flag  whether the imeage belongs to a profile or project
+     * @param  String $path the path of the current image
+     * @param  String $fileName the filename of the string
+     */
+    private function resize($image, $flag, $path, $fileName)
     {
-    	if($profile) {
-	    	Intervention::make($image)->fit(50, 50)->save($this->path . '/sm_' . $fileName;
-	    	Intervention::make($image)->fit(200, 200)->save($this->path . '/lg_' . $fileName);
+    	// image resizing for profiles
+    	if($flag) {
+	    	Intervention::make($image)->fit(50, 50)->save($path . '/sm_' . $fileName);
+	    	Intervention::make($image)->fit(200, 200)->save($path . '/lg_' . $fileName);
+	    // image resizing for project images
     	} else {
-	    	Intervention::Make($image)->fit(150, 150)->save($this->path . '/sm_' . $fileName);
-            Intervention::make($image)->fit(200, 175)->save($this->path . '/lg_' . $fileName);
+	    	Intervention::Make($image)->fit(150, 150)->save($path . '/sm_' . $fileName);
+            Intervention::make($image)->fit(200, 175)->save($path . '/lg_' . $fileName);
     	}
     }
 
-    private function traverseImages()
+    /**
+     * This traverses the files based on a globally defined path
+     * @param String $path the path of the current file
+     * @param  boolean $bool determines between re-sizing useror profile images
+     */
+    private function traverseImages($path, $bool)
     {
-    	$files = File::files($this->path);
+    	$files = File::files($path);
+    	// to hold the images we do not want to get deleted
+    	$tmp = [];
     	foreach($files as $file) {
-    		if(!strpos($file, 'placeholder')) {
-    			$this->resize($file, true, str_replace($this->path . '/', '', $file));
+    		// check to see if images that have the sm_ lg_ or placeholder on the image name
+    		if(!strpos($file, 'placeholder') && !strpos($file, 'sm_') && !strpos($file, 'lg_')) {
+    			$this->resize($file, $bool, $path, str_replace($path . '/', '', $file));
+    		} else {
+    			// add the files we want to keep
+    			$tmp [] = $file;
     		}
     	}
+
+    	// We delete the files ignoring the ones we want to keep
+    	$files = array_diff($files, $tmp);
+    	File::delete($files);
     }
 
+    /**
+     * The index page that does the image re-sizing
+     * @return String the success string to the user
+     */
     public function index()
     {
-    	$this->path = 'user-profile/image';
-    	$this->traverseImages();
+    	// trigger the user-profile resizing
+    	$path = 'user-profile/image';
+    	$this->traverseImages($path, true);
 
-    	$this->path = 'imgs/projects';
-    	$this->traverseImages();
+    	// trigger the project image resizing
+    	$path = 'imgs/projects';
+    	$this->traverseImages($path, false);
+
+    	return "<h1>Successfully re-sized all images</h1>";
     }
 }
