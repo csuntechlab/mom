5d3a514d1be8e32f11632c0750ffa027ad76ed4c
ats88641 <Art.Sintoplertchaiyakul.909@my.csun.edu>
2016-08-29 12:20:09 -0700

    Reimplemented collapse functionality

--- resources/views/pages/projects/work.blade.php
+++ resources/views/pages/projects/work.blade.php
@@ -49,87 +49,94 @@ Our Work
                                         Meta+Lab
                                         @endif
                                     </span>
+                                    <div class="row visible-xs visible-sm visible-md">
+                                        <div class="col-xs-12">
+                                            <button class="btn btn--view btn-sm btn-primary center-block" type="button" data-toggle="collapse" data-target="#collapse{{ $y }}" aria-expanded="false" aria-controls="collapseExample">View +</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                         
-                        <div class="col-xs-12 col-sm-12 col-md-4">
-                            <div class="card card-light">
-                                <div class="card-content-xl card-content-action">
-                                    <div class="row">
-                                        <div class="col-xs-6 col-sm-6 col-md-12">
-                                            <h3>Scope of Work</h3>
-                                        </div>
-                                        <div class="col-xs-12 col-sm-12 col-md-12">
-                                            <p>{{$project->meta->description}}</p>
-                                        </div>
-                                        <div class="col-xs-6 col-md-6">
-                                            <h2 class="small">
-                                                Product Owner
-                                            </h2>
-                                        </div>
-                                        @if($project->scrumMaster->first_name != null)
+                        <div class="col-xs-12 col-sm-12 col-md-8 project-collapse collapse" id="collapse{{ $y }}">
+                            <div class="col-xs-12 col-sm-12 col-md-6">
+                                <div class="card card-light">
+                                    <div class="card-content-xl card-content-action">
+                                        <div class="row">
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <h3>Scope of Work</h3>
+                                            </div>
+                                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                                <p>{{$project->meta->description}}</p>
+                                            </div>
                                             <div class="col-xs-6 col-md-6">
                                                 <h2 class="small">
-                                                    Scrum Master
+                                                    Product Owner
                                                 </h2>
                                             </div>
-                                        @endif
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-xs-6 col-md-6">
-                                            <a href="{{$project->productOwner->profile_link}}">
-                                                @if(!empty($project->productOwner->profile->image))
-                                                    <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->productOwner->profile->image)}}" alt="{{$project->productOwner->first_name}}">
-                                                @else
-                                                    <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->productOwner->first_name}}">
-                                                @endif
-                                                    <div class="members--member-name">{{$project->productOwner->first_name}}</div>
-                                            </a>
+                                            @if($project->scrumMaster->first_name != null)
+                                                <div class="col-xs-6 col-md-6">
+                                                    <h2 class="small">
+                                                        Scrum Master
+                                                    </h2>
+                                                </div>
+                                            @endif
                                         </div>
 
-                                        @if($project->scrumMaster->first_name != null)
-                                            <div class="col-md-6">
-                                                <a href="{{ $project->scrumMaster->profile_link ?: '#' }}">
-                                                    @if(!empty($project->scrumMaster->profile->image))
-                                                        <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->scrumMaster->profile->image)}}" alt="{{$project->scrumMaster->first_name}}">
+                                        <div class="row">
+                                            <div class="col-xs-6 col-md-6">
+                                                <a href="{{$project->productOwner->profile_link}}">
+                                                    @if(!empty($project->productOwner->profile->image))
+                                                        <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->productOwner->profile->image)}}" alt="{{$project->productOwner->first_name}}">
                                                     @else
-                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="project-scrum-master">
+                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$project->productOwner->first_name}}">
                                                     @endif
-                                                    <div class="members--member-name">
-                                                        {{ $project->scrumMaster->first_name or 'N/A' }}
-                                                    </div>
+                                                        <div class="members--member-name">{{$project->productOwner->first_name}}</div>
                                                 </a>
                                             </div>
-                                         @endif
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xs-12 col-sm-12 col-md-4">
-                            <div class="card card-light">
-                                <div class="card-content-xl card-content-action">
-                                    <div class="row">
-                                        <h3>Team Members</h3>
-                                    </div>
-                                    <div class="members">
-                                        @foreach($project->members as $member)
-                                                <a href="{{ $member->profile_link }}" class="members--member">
-                                                @if(!empty($member->profile) && !empty($member->profile->image))
-                                                    <img class="members--member-img" src="{{ asset('user-profile/image/' . 'sm_' . $member->profile->image->src)}}" alt="{{$member->first_name}}">
-                                                @else
-                                                    <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$member->first_name}}">
-                                                @endif
-                                                <div class="members--member-name">{{$member->first_name}}</div>
-                                            </a>
-                                        @endforeach
 
+                                            @if($project->scrumMaster->first_name != null)
+                                                <div class="col-md-6">
+                                                    <a href="{{ $project->scrumMaster->profile_link ?: '#' }}">
+                                                        @if(!empty($project->scrumMaster->profile->image))
+                                                            <img class="members--member-img" src="{{ asset('user-profile/image/' . $project->scrumMaster->profile->image)}}" alt="{{$project->scrumMaster->first_name}}">
+                                                        @else
+                                                            <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="project-scrum-master">
+                                                        @endif
+                                                        <div class="members--member-name">
+                                                            {{ $project->scrumMaster->first_name or 'N/A' }}
+                                                        </div>
+                                                    </a>
+                                                </div>
+                                             @endif
+                                        </div>
                                     </div>
                                 </div>
                             </div>
+                            <div class="col-xs-12 col-sm-12 col-md-6">
+                                <div class="card card-light">
+                                    <div class="card-content-xl card-content-action">
+                                        <div class="row">
+                                            <h3>Team Members</h3>
+                                        </div>
+                                        <div class="members">
+                                            @foreach($project->members as $member)
+                                                    <a href="{{ $member->profile_link }}" class="members--member">
+                                                    @if(!empty($member->profile) && !empty($member->profile->image))
+                                                        <img class="members--member-img" src="{{ asset('user-profile/image/' . 'sm_' . $member->profile->image->src)}}" alt="{{$member->first_name}}">
+                                                    @else
+                                                        <img class="members--member-img" src="{{ asset('/imgs/anonymous.png') }}" alt="{{$member->first_name}}">
+                                                    @endif
+                                                    <div class="members--member-name">{{$member->first_name}}</div>
+                                                </a>
+                                            @endforeach
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php $y++; ?> 
                         </div>
-                        <?php $y++; ?>  
                     </div>
                 </div>
             </div>
@@ -146,6 +153,7 @@ $(document).ready(windowResize);
 $(window).resize(windowResize);
 
 function windowResize() {
+    console.log($(window).width());
     if($(window).width() > 991){
         for(y=1; y <= projects; y++){
             var radius = 130;
